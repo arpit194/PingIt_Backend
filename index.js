db642913fe3e7bd5882fa0acdb3c09a9d2294928
Arpit Patel <arpit.patel194@gmail.com>
2022-08-20 12:41:02 +0530

    allow all origins

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://arpit194.github.io/PingIt"],
+    // origin: ["http://localhost:3000", "https://arpit194.github.io/PingIt"],
     credentials: true,
   },
 });
